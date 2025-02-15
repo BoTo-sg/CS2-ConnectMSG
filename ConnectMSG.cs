17a4eb2491c5f7de877d909d199876c885a13acc
BoTo-sg <67488940+BoTo-sg@users.noreply.github.com>
2025-02-15 15:28:50 -0500

    Update ConnectMSG.cs

--- ConnectMSG.cs
+++ ConnectMSG.cs
@@ -137,7 +137,7 @@ public class ConnectMSG : BasePlugin, IPluginConfig<ConnectMSGConfig>
         try
         {
             using var reader = new DatabaseReader(Path.Combine(ModuleDirectory, "GeoLite2-Country.mmdb"));
-            var response = reader.Country(ipAddress);
+            var response = reader.City(ipAddress);
             return response?.Country?.IsoCode ?? "Unknown";
         }
         
