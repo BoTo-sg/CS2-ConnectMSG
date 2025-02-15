03a3b158e0b3449c950cb25c3fee4b310ca0d39c
BoTo-sg <67488940+BoTo-sg@users.noreply.github.com>
2025-02-15 16:55:19 -0500

    Update ConnectMSG.cs

--- ConnectMSG.cs
+++ ConnectMSG.cs
@@ -185,8 +185,8 @@ public class ConnectMSG : BasePlugin, IPluginConfig<ConnectMSGConfig>
                 {
                     Logger.LogInformation($"Failed to send message to Discord! code: {response.StatusCode}");
                 }
-            });
-        }      
+            }
+        }
         catch
         {
         
