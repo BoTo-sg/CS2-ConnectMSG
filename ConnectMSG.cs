a098f440ee969b723889e7d430841188f4da942b
BoTo-sg <67488940+BoTo-sg@users.noreply.github.com>
2025-02-16 05:09:40 -0500

    Update ConnectMSG.cs

--- ConnectMSG.cs
+++ ConnectMSG.cs
@@ -89,15 +89,6 @@ public class ConnectMSG : BasePlugin, IPluginConfig<ConnectMSGConfig>
             return HookResult.Continue;
 
         info.DontBroadcast = true;
-        return HookResult.Continue;
-    }
-
-    [GameEventHandler]
-    private HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
-    {
-        if (@event == null)
-            return HookResult.Continue;
-
         var player = @event.Userid;
 
         if (player == null || !player.IsValid || player.IsBot)
@@ -123,8 +114,6 @@ public class ConnectMSG : BasePlugin, IPluginConfig<ConnectMSGConfig>
                 return HookResult.Continue;
             }
         }
-
-        info.DontBroadcast = true;
 
         //Console.WriteLine($"[{ModuleName}] {Name} has disconnected!");
         //Console.WriteLine($"[{ModuleName}] {Name} [{steamid2}] has disconnected from {country} ({disconnectReason})!");
